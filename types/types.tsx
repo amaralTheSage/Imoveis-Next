b98b3db3e4155a985876e5564f41f257aa654707
amaralTheSage <amaraldesouza9@gmail.com>
2024-04-20 11:29:52 -0300

    remoçao do estado e +test data

--- types/types.tsx
+++ types/types.tsx
@@ -1,17 +1,16 @@
 export interface Property {
-    id: number;
-    name: string;
-    type: "house" | "apartment" | "land" | "camp";
-    city: string;
-    state: string;
-    neighborhood: string;
-    description: string;
-    images: string[];
-    area: number;
-    bedrooms: number;
-    bathrooms: number;
-    car_spots: number;
-    price: number;
-    rent: number;
-    condo_price: number;
-  }
+  id: number;
+  name: string;
+  type: "house" | "apartment" | "urban land" | "rural land";
+  city: string;
+  neighborhood: string;
+  description: string;
+  images: string[];
+  area: number;
+  bedrooms: number;
+  bathrooms: number;
+  car_spots: number;
+  price: number;
+  rent: number;
+  condo_price: number;
+}
